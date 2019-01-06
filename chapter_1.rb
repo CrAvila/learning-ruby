e9268d0b2f8277c60d8eed1f71c4634b60598f32
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-06 15:44:38 -0600

    Complete chapter 1 Exercises

---
+++ chapter_1.rb
@@ -0,0 +1,29 @@
+puts "Learn to Program"
+puts "Chapter 1"
+puts ""
+
+puts "How many hours are in a year?"
+hours_in_a_year = 24 * 365
+puts hours_in_a_year
+puts ""
+
+puts "How many minutes are in a decade?"
+hours_in_a_day = 24
+minutes_in_a_day = hours_in_a_day * 60
+minutes_in_a_year = minutes_in_a_day * 365
+minutes_in_a_decade = minutes_in_a_year * 10
+puts minutes_in_a_decade
+puts ""
+
+puts "How many seconds old are you?"
+seconds_in_a_year = minutes_in_a_year * 60
+puts seconds_in_a_year * 17
+puts ""
+
+puts "How many chocolates do you hope to eat in your life?"
+    puts "Bruh, I'm diabetic lmao"
+puts ""
+
+seconds_old = 1298000000
+puts "If I am #{seconds_old} seconds old, how old I am?"
+puts seconds_old/seconds_in_a_year
