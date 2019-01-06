fe33734b5e46e5823d67a0d936905e08843f6544
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-06 15:14:22 -0600

    Add favorite number

--- student.rb
+++ student.rb
@@ -1,12 +1,18 @@
 class Student
     attr_accessor :first_name, :last_name, :primary_phone_number
 
-    def introduction
-        puts "Hi, I'm #{first_name}!"
+    def introduction(target)
+        puts "Hi #{target}, I'm #{first_name}!"
     end
+
+    def favorite_number
+        7
+    end
+
 
 end
 
 frank = Student.new
 frank.first_name = "Frank"
-frank.introduction
+frank.introduction('Katrina')
+puts "#{frank.first_name}'s favorite number is #{frank.favorite_number}"
