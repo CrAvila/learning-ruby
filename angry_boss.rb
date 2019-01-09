92deffee501e1fb49ad170f27783566644cc122a
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-08 22:12:10 -0600

    Add angry boss program

---
+++ angry_boss.rb
@@ -0,0 +1,4 @@
+puts "WHAT THE HELL DO YOU WANT?!"
+answer = gets.chomp
+puts "WHAT DO YOU MEAN BY \"#{answer.upcase}\"?!?! YOU'RE FIRED!!"
+
---
+++ name_length.rb
@@ -0,0 +1,10 @@
+puts "What's your name?"
+puts "Enter your first name:"
+first_name = gets.chomp
+puts "Enter your second name:"
+second_name = gets.chomp
+puts "Enter your first last name:"
+last_name = gets.chomp
+puts "Enter your second last nem:"
+second_last_name = gets.chomp
+puts "Did you know your name has #{first_name.length+second_name.length+last_name.length+second_last_name.length} letters?"
