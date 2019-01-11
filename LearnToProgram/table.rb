7a993e66ad9c0919308c2ab1dc6b09815af237fd
CR_Avila <carlosrobertoavilah@gmail.com>
2019-01-10 23:49:45 -0600

    Add LTP Chapter 5 exercises

---
+++ LearnToProgram/table.rb
@@ -0,0 +1,5 @@
+w = 50
+puts "Table of contents".center w
+puts "Chapter 1: Numbers".ljust(w/2) + "page 1".rjust(w/2) 
+puts "Chapter 2: Letters".ljust(w/2) + "page 1".rjust(w/2) 
+puts "Chapter 3: Variables".ljust(w/2) + "page 1".rjust(w/2) 
